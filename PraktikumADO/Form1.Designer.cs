d402459c9fc59353ff78b878f5b67dcf16e05148
ranggarap <fadhilahrangga26@gmail.com>
2026-03-31 17:12:18 +0700

    menambahkan namespace dan variabel koneksi

--- PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.btnConnect.TabIndex = 0;
             this.btnConnect.Text = "connect";
             this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
             // btnHitungMhs
             // 
